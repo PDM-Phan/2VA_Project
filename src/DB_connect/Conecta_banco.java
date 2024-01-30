c910d8a15781b4962391bd0617d5d3c20fd1100d
PDM-Phan <pedroddemelo@gmail.com>
2024-01-29 21:38:45 -0300

    Creating class to start connection with db

---
+++ src/DB_connect/Conecta_banco.java
@@ -0,0 +1,22 @@
+package DB_Connect;
+import java.sql.Connection; //lib do jdbc do java para a conexao com o banco
+import java.sql.DriverManager; 
+
+public class Conecta_banco {
+    private Connection con;
+    private String driver = "com.mysql.cj.jdbc.Driver";
+    private String url = "jdbc:mysql://localhost:3306/db2va";
+    private String user = "root";
+    private String password = "Macacoalado1!";
+    
+    public Connection conectar() {
+        try {
+            Class.forName(driver);
+            con = DriverManager.getConnection(url, user, password);
+            return con;
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+}
